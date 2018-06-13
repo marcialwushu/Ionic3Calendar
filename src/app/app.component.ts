9b8a80ff29cc1245e71a8ee10432614e60f7dfb5
marcialwushu <marcialwushu@gmail.com>
2018-06-13 14:56:59 -0300

    LoginPage added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
 import { timer } from 'rxjs/observable/timer';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'LoginPage';
 
   showSplash = true;
 
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -20,6 +16,18 @@ export class LoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login(){
+    this.navCtrl.push(HomePage);
+  }
+
+  forgotPwd(){
+
+  }
+
+  signUp(){
+
   }
 
 }
