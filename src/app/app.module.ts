3bfe5a9069917fd29708bf379d23b306a2fb57cd
marcialwushu <marcialwushu@gmail.com>
2018-06-14 16:40:01 -0300

    create resetpasswordPage and signupPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginPage } from './../pages/login/login';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
@@ -11,8 +12,8 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { NgCalendarModule } from 'ionic2-calendar';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
-import { AuthService } from './../providers/auth/auth-service';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNoLztCehx8evMmIECxjepabQno-dUt8s",
@@ -26,23 +27,28 @@ const firebaseConfig = {
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     NgCalendarModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthService
+    AuthServiceProvider,
+
   ]
 })
 export class AppModule {}
